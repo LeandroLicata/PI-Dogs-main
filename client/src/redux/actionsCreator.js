814686b638da9aafb1960be6c4e3847b1dfca6ac
LeandroLicata <leandrolicata1@gmail.com>
2023-01-13 09:38:06 -0300

    front removed

--- client/src/redux/actionsCreator.js
+++
@@ -1,9 +0,0 @@
-export const GET_DOGS = "GET_DOGS";
-export const GET_TEMPERAMENTS = "GET_TEMPERAMENTS";
-export const FILTER_BY_TEMPERAMENTS = "FILTER_BY_TEMPERAMENTS";
-export const FILTER_BY_ORIGIN = "FILTER_BY_ORIGIN";
-export const SORT_BY_NAME = "SORT_BY_NAME";
-export const SORT_BY_WEIGHT = "SORT_BY_WEIGHT";
-export const GET_DOGS_BY_NAME = "GET_DOGS_BY_NAME";
-export const POST_DOG = "POST_DOG";
-export const GET_DETAILS = "GET_DETAILS";
